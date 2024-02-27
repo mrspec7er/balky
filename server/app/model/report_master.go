d83794377c7aaaa341bd7a6648804793e7c3c3a6
Kusuma Sandi <wijayakusumasandi@gmail.com>
2024-02-27 14:47:25 +0800

    feat: 🎸 added report master model utility

--- server/app/model/report_master.go
+++ server/app/model/report_master.go
@@ -3,6 +3,7 @@ package model
 import (
 	"time"
 
+	"github.com/mrspec7er/balky/app/utils"
 	"gorm.io/gorm"
 )
 
@@ -16,4 +17,24 @@ type ReportMaster struct {
 
 	Applications []*Application `json:"applications"`
 	Attributes   []*Attribute   `json:"attributes"`
+}
+
+func (m *ReportMaster) store() *gorm.DB {
+	return utils.DB
+}
+
+func (m *ReportMaster) Create() error {
+	err := m.store().Create(&m).Error
+	return err
+}
+
+func (m *ReportMaster) FindMany() ([]ReportMaster, error) {
+	masters := []ReportMaster{}
+	err := m.store().Find(&masters).Error
+	return masters, err
+}
+
+func (m *ReportMaster) Delete() error {
+	err := m.store().Delete(&m).Error
+	return err
 }
