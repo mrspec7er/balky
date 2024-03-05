cf96f3f1a0c00e252b4f604469ad5dd861845f43
Kusuma Sandi <wijayakusumasandi@gmail.com>
2024-03-05 22:31:47 +0800

    feat: 🎸 added content model utility

--- server/app/model/content.go
+++ server/app/model/content.go
@@ -1,5 +1,10 @@
 package model
 
+import (
+	"github.com/mrspec7er/balky/app/utility"
+	"gorm.io/gorm"
+)
+
 type Content struct {
 	ApplicationNumber string       `gorm:"primaryKey"`
 	Application       *Application `json:"application" gorm:"foreignKey:ApplicationNumber"`
@@ -8,4 +13,24 @@ type Content struct {
 	Attribute   *Attribute `json:"attribute"`
 
 	Value string `json:"value" gorm:"type:text"`
+}
+
+func (a *Content) store() *gorm.DB {
+	return utility.DB
+}
+
+func (a *Content) Create() error {
+	err := a.store().Create(&a).Error
+	return err
+}
+
+func (a *Content) FindMany() ([]*Content, error) {
+	contents := []*Content{}
+	err := a.store().Find(&contents).Error
+	return contents, err
+}
+
+func (a *Content) Delete() error {
+	err := a.store().Delete(&a).Error
+	return err
 }
