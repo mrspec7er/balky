efca067c27cfc4b139df00b80018901ecd681390
Kusuma Sandi <wijayakusumasandi@gmail.com>
2024-03-05 22:09:58 +0800

    feat: 🎸 added application model utility

--- server/app/model/application.go
+++ server/app/model/application.go
@@ -3,6 +3,7 @@ package model
 import (
 	"time"
 
+	"github.com/mrspec7er/balky/app/utils"
 	"gorm.io/gorm"
 )
 
@@ -20,4 +21,29 @@ type Application struct {
 
 	UserID uint
 	User   *User `json:"user"`
+}
+
+func (a *Application) store() *gorm.DB {
+	return utils.DB
+}
+
+func (a *Application) Create() error {
+	err := a.store().Create(&a).Error
+	return err
+}
+
+func (a *Application) FindMany() ([]*Application, error) {
+	applications := []*Application{}
+	err := a.store().Find(&applications).Error
+	return applications, err
+}
+
+func (a *Application) FindOne() (*Application, error) {
+	err := a.store().Where("number = ?", a.Number).First(&a).Error
+	return a, err
+}
+
+func (a *Application) Delete() error {
+	err := a.store().Delete(&a).Error
+	return err
 }
