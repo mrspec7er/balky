2af5404b17126e6d123f1d509f87652207b37e2c
Kusuma Sandi <wijayakusumasandi@gmail.com>
2024-03-06 22:17:07 +0800

    feat: 🎸 added app and content listeners

--- server/app/module/application/listener.go
+++ server/app/module/application/listener.go
@@ -3,35 +3,146 @@ package application
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"sync"
 
 	"github.com/mrspec7er/balky/app/model"
+	"github.com/mrspec7er/balky/app/module/logger"
 	"github.com/rabbitmq/amqp091-go"
 )
 
 type ApplicationListener struct {
 	service ApplicationService
+	logger  logger.LoggerService
 }
 
-func (l ApplicationListener) Create(queue *amqp091.Channel, wg *sync.WaitGroup, queueName string, consumerTag string) {
+func (l *ApplicationListener) Create(queue *amqp091.Channel, wg *sync.WaitGroup, queueName string, consumerTag string) {
 	defer wg.Done()
 
 	ctx := context.Background()
 	messages, err := queue.ConsumeWithContext(ctx, queueName, consumerTag, true, false, false, false, nil)
 
 	if err != nil {
-		fmt.Println(err)
+		l.logger.Publish("Server", 500, err.Error())
 	}
 
 	for data := range messages {
-		app := &model.Application{}
+		application := &model.Application{}
 
-		err := json.Unmarshal(data.Body, &app)
-		if err != nil {
-			fmt.Println(err)
+		userEmail, ok := data.Headers["userEmail"].(string)
+		if !ok {
+			l.logger.Publish(userEmail, 400, "Missing user credentials")
+			continue
 		}
 
-		l.service.Create(app)
+		err := json.Unmarshal(data.Body, &application)
+		if err != nil {
+			l.logger.Publish(userEmail, 400, err.Error())
+			continue
+		}
+
+		status, err := l.service.Create(application)
+		if err != nil {
+			l.logger.Publish(userEmail, status, err.Error())
+			continue
+		}
+	}
+}
+
+func (l *ApplicationListener) Delete(queue *amqp091.Channel, wg *sync.WaitGroup, queueName string, consumerTag string) {
+	defer wg.Done()
+
+	ctx := context.Background()
+	messages, err := queue.ConsumeWithContext(ctx, queueName, consumerTag, true, false, false, false, nil)
+
+	if err != nil {
+		l.logger.Publish("Server", 500, err.Error())
+	}
+
+	for data := range messages {
+		application := &model.Application{}
+
+		userEmail, ok := data.Headers["userEmail"].(string)
+		if !ok {
+			l.logger.Publish(userEmail, 400, "Missing user credentials")
+			continue
+		}
+
+		err := json.Unmarshal(data.Body, &application)
+		if err != nil {
+			l.logger.Publish(userEmail, 400, err.Error())
+			continue
+		}
+
+		status, err := l.service.Delete(application)
+		if err != nil {
+			l.logger.Publish(userEmail, status, err.Error())
+			continue
+		}
+	}
+}
+
+func (l *ApplicationListener) CreateContent(queue *amqp091.Channel, wg *sync.WaitGroup, queueName string, consumerTag string) {
+	defer wg.Done()
+
+	ctx := context.Background()
+	messages, err := queue.ConsumeWithContext(ctx, queueName, consumerTag, true, false, false, false, nil)
+
+	if err != nil {
+		l.logger.Publish("Server", 500, err.Error())
+	}
+
+	for data := range messages {
+		contents := []*model.Content{}
+
+		userEmail, ok := data.Headers["userEmail"].(string)
+		if !ok {
+			l.logger.Publish(userEmail, 400, "Missing user credentials")
+			continue
+		}
+
+		err := json.Unmarshal(data.Body, &contents)
+		if err != nil {
+			l.logger.Publish(userEmail, 400, err.Error())
+			continue
+		}
+
+		status, err := l.service.CreateContent(contents)
+		if err != nil {
+			l.logger.Publish(userEmail, status, err.Error())
+			continue
+		}
+	}
+}
+
+func (l *ApplicationListener) DeleteContent(queue *amqp091.Channel, wg *sync.WaitGroup, queueName string, consumerTag string) {
+	defer wg.Done()
+
+	ctx := context.Background()
+	messages, err := queue.ConsumeWithContext(ctx, queueName, consumerTag, true, false, false, false, nil)
+
+	if err != nil {
+		l.logger.Publish("Server", 500, err.Error())
+	}
+
+	for data := range messages {
+		content := &model.Content{}
+
+		userEmail, ok := data.Headers["userEmail"].(string)
+		if !ok {
+			l.logger.Publish(userEmail, 400, "Missing user credentials")
+			continue
+		}
+
+		err := json.Unmarshal(data.Body, &content)
+		if err != nil {
+			l.logger.Publish(userEmail, 400, err.Error())
+			continue
+		}
+
+		status, err := l.service.DeleteContent(content)
+		if err != nil {
+			l.logger.Publish(userEmail, status, err.Error())
+			continue
+		}
 	}
 }
--- server/app/module/application/router.go
+++ server/app/module/application/router.go
@@ -12,4 +12,13 @@ func HandlerConfig(q *amqp091.Channel, wg *sync.WaitGroup) {
 
 	wg.Add(1)
 	go l.Create(q, wg, "app.create", "app.create."+os.Getenv("SERVER_ID"))
+
+	wg.Add(1)
+	go l.Delete(q, wg, "app.delete", "app.delete."+os.Getenv("SERVER_ID"))
+
+	wg.Add(1)
+	go l.CreateContent(q, wg, "content.create", "content.create."+os.Getenv("SERVER_ID"))
+
+	wg.Add(1)
+	go l.DeleteContent(q, wg, "content.delete", "content.delete."+os.Getenv("SERVER_ID"))
 }
