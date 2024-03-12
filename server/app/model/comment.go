8bcc7b6ba1824a6202143b8a77e3ee327ac1729b
Kusuma Sandi <wijayakusumasandi@gmail.com>
2024-03-12 22:42:59 +0800

    feat: 🎸 added comment utility

--- server/app/model/comment.go
+++ server/app/model/comment.go
@@ -3,6 +3,7 @@ package model
 import (
 	"time"
 
+	"github.com/mrspec7er/balky/app/utility"
 	"gorm.io/gorm"
 )
 
@@ -13,9 +14,21 @@ type Comment struct {
 	UpdatedAt time.Time      `json:"updatedAt"`
 	DeletedAt gorm.DeletedAt `json:"deletedAt" gorm:"index"`
 
-	UserEmail string `json:"userEmail"`
-	User      *User  `json:"user" gorm:"foreignKey:UserEmail"`
+	UserEmail string   `json:"userEmail"`
+	User      *Comment `json:"user" gorm:"foreignKey:UserEmail"`
 
 	ReferenceID uint     `json:"referenceId"`
 	Reference   *Comment `json:"reference"`
+}
+
+func (c *Comment) store() *gorm.DB {
+	return utility.DB
+}
+
+func (c *Comment) Create() error {
+	return c.store().Create(&c).Error
+}
+
+func (c *Comment) Delete() error {
+	return c.store().Delete(&c).Error
 }
